a8b4212c1b41bd40c3aac7f0e051d9a4636a942a
hong <98158962@qq.com>
2026-02-24 23:18:19 +0800

    Test Husky2

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,11 +9,11 @@ function App() {
          .then((data) => setMessage(data.message));
    }, []);
    return (
-<div className="p-4">
+      <div className="p-4">
          <p className="font-bold text-4xl">{message}</p>
          <Button>Click me!</Button>
       </div>
-   )
+   );
 }
 
 export default App;
