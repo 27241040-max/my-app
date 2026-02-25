9455eab37e4d7aed70812ece012ff50ca55adb27
hong <98158962@qq.com>
2026-02-25 13:16:22 +0800

    Extracting chat service

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import type { Request, Response } from 'express';
+import z from 'zod';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -12,6 +16,32 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World' });
+});
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, 'Prompt is too long(max 1000 characters'),
+   conversationId: z.string().uuid(),
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+   }
+
+   try {
+      const { prompt, conversationId } = req.body;
+      const response = await chatService.sendMessage(prompt, conversationId);
+
+      res.json({ message: response.message });
+   } catch (error) {
+      res.status(500).json({ error: 'Failed to generate a response.' });
+   }
 });
 
 app.listen(port, () => {
