c580b53344a2f04a1b4509c805bfe1e1a2e92035
hong <98158962@qq.com>
2026-02-27 14:27:05 +0800

    Storing-the-Summary

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -2,7 +2,6 @@ import { type Review } from '../generated/prisma';
 import { reviewRepository } from '../repositories/review.repository';
 import { llmClient } from '../llm/client';
 import template from '../prompts/summarize-reviews.txt';
-import { join } from '../generated/prisma/runtime/library';
 
 export const reviewService = {
    async getReview(productId: number): Promise<Review[]> {
@@ -12,13 +11,15 @@ export const reviewService = {
       const reviews = await reviewRepository.getReviews(productId, 10);
       const joinedReviews = reviews.map((r) => r.content).join('\n\n');
       const prompt = template.replace('{{reviews}}', joinedReviews);
-      const response = await llmClient.generateText({
+      const { text: summary } = await llmClient.generateText({
          model: 'gpt-4.1',
          prompt,
          temperature: 0.2,
          maxTokens: 500,
       });
 
-      return response.text;
+      await reviewRepository.storeReviewSummary(productId, summary);
+
+      return summary;
    },
 };
