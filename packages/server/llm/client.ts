d0de640acb008edafdfa3650b43496f596be1f93
hong <98158962@qq.com>
2026-02-27 13:53:59 +0800

    Refactoring-Extracting-the-LIm-Logic

---
+++ packages/server/llm/client.ts
@@ -0,0 +1,43 @@
+import OpenAI from 'openai';
+import { text } from 'stream/consumers';
+
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
+type GenerateTextOption = {
+   model?: string;
+   prompt: string;
+   instructions?: string;
+   temperature?: number;
+   maxTokens?: number;
+   previousResponseId?: string;
+};
+type GererateTextResult = {
+   id: string;
+   text: string;
+};
+
+export const llmClient = {
+   async generateText({
+      model = 'gpt-4.1',
+      prompt,
+      instructions,
+      temperature = 0.2,
+      maxTokens = 300,
+      previousResponseId,
+   }: GenerateTextOption): Promise<GenerateTextResult> {
+      const response = await client.responses.create({
+         model,
+         input: prompt,
+         instructions,
+         temperature,
+         max_output_tokens: maxTokens,
+         previous_response_id: previousResponseId,
+      });
+      return {
+         id: response.id,
+         text: response.output_text,
+      };
+   },
+};
