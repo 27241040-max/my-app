893a70442304976c366fad69c40fbaae0b846b40
hong <98158962@qq.com>
2026-02-27 13:59:01 +0800

    Refactoring-Extracting-the-Prompt

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,6 +1,8 @@
 import { type Review } from '../generated/prisma';
 import { reviewRepository } from '../repositories/review.repository';
 import { llmClient } from '../llm/client';
+import template from '../prompts/summarize-reviews.txt';
+import { join } from '../generated/prisma/runtime/library';
 
 export const reviewService = {
    async getReview(productId: number): Promise<Review[]> {
@@ -9,12 +11,7 @@ export const reviewService = {
    async summarizeReviews(productId: number): Promise<string> {
       const reviews = await reviewRepository.getReviews(productId, 10);
       const joinedReviews = reviews.map((r) => r.content).join('\n\n');
-      const prompt = `
-      Summarize thefollowing customer reviews into a short 
-      paragraphhighlighting key themes, both positive and negative:
-      
-      ${joinedReviews}
-      `;
+      const prompt = template.replace('{{reviews}}', joinedReviews);
       const response = await llmClient.generateText({
          model: 'gpt-4.1',
          prompt,
