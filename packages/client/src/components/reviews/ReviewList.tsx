39c94673c36499215b68e5c9480c74924c94efb5
hong <98158962@qq.com>
2026-03-02 10:36:49 +0800

    Displaying-the-Summary

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import StarRating from './StarRating';
 import Skeleton from 'react-loading-skeleton';
+import { HiSparkles } from 'react-icons/hi2';
 import { useQuery } from '@tanstack/react-query';
+import { Button } from '../ui/button';
 
 type Props = {
    productId: number;
@@ -52,18 +54,32 @@ const ReviewList = ({ productId }: Props) => {
    if (error) {
       return <p className="text-red-500">无法获得评论，请重试</p>;
    }
-
+   if (!reviewData?.reviews.length) {
+      return null;
+   }
    return (
-      <div className="flex flex-col gap-5">
-         {reviewData?.reviews.map((review) => (
-            <div key={review.id}>
-               <div className="font-semibold">{review.author}</div>
-               <div>
-                  <StarRating value={review.rating} />
+      <div>
+         <div className="mb-5">
+            {reviewData?.summary ? (
+               <p>{reviewData?.summary}</p>
+            ) : (
+               <Button>
+                  <HiSparkles />
+                  Summarize
+               </Button>
+            )}
+         </div>
+         <div className="flex flex-col gap-5">
+            {reviewData?.reviews.map((review) => (
+               <div key={review.id}>
+                  <div className="font-semibold">{review.author}</div>
+                  <div>
+                     <StarRating value={review.rating} />
+                  </div>
+                  <p className="py-2">{review.content}</p>
                </div>
-               <p className="py-2">{review.content}</p>
-            </div>
-         ))}
+            ))}
+         </div>
       </div>
    );
 };
